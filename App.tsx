5b25d646f171981c6f62d804e7f8bbc755b21bd2
ThomasJanko <thomas78125@gmail.com>
2023-03-30 10:37:43 +0200

    redux init

--- App.tsx
+++ App.tsx
@@ -2,13 +2,19 @@ import React, { useEffect } from 'react';
 import { theme } from './src/config/theme'
 import { ThemeProvider } from 'styled-components';
 import Routes from './src/config/routes';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './src/redux/reducer';
 
+const store = createStore(reducer);
 
 function App() {
  
   return (
     <ThemeProvider theme={theme}>
+      <Provider store={store}>
         <Routes/>
+      </Provider>
     </ThemeProvider>
   );
 }
