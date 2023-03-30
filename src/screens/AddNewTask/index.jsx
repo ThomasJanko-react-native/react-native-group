14693a8ce62b2667979010805e685345b7ac0029
kouci747 <koceila.arhab@gmail.com>
2023-03-30 12:11:35 +0200

    commit 1

---
+++ src/screens/AddNewTask/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+function AddNewTask() {
+  return (
+    <View>
+      <Text>Add New Task</Text>
+    </View>
+  );
+}
+
+export default AddNewTask;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import TodoList from '../components/TodoList';
+import AddNewTask from './AddNewTask';
+//import AddNewTask from './AddNewTask';
 
 const HomeScreen = () => {
-    return (
-        <View style={{backgroundColor: 'grey', flex: 1}}>
-            <Text>Home Screen</Text>
-            <TodoList/>
-        </View>
-    );
-}
-
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <TodoList />
+      <AddNewTask />
+    </View>
+  );
+};
 
 export default HomeScreen;
