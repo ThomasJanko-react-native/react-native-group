f2118b9cbaf1b34528a0da193c396412a2a11fb2
ThomasJanko <thomas78125@gmail.com>
2023-04-03 15:37:54 +0200

    dark / light theme

--- src/screens/AddNewTaskScreen.jsx
+++ src/screens/AddNewTaskScreen.jsx
@@ -18,8 +18,7 @@ function AddNewTaskScreen() {
       <Spacer height={40} />
       <AddTaskStepComp title={'Write your task here...'} />
       <Spacer height={10} />
-      {/* <AddTaskStepComp title={'Write your task here...'} />
-      <Spacer height={10} /> */}
+      
       <AddNewStepBtn />
       <Spacer height={30} />
       <AddTaskTimeComp />
@@ -40,7 +39,7 @@ const Title = styled.Text`
   font-weight: bold;
   font-size: 24px;
   text-align: center;
-  color: black;
+  color: ${props => props.theme.textColor};
 `;
 
 const Spacer = styled.View`
