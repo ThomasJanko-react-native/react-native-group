3f7db196f23a4bab6ac3ae8e4485bf0477de03d9
thomas78125 <thomas78125@gmail.com>
2023-04-10 12:44:34 +0200

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,28 @@
-import {combineReducers, createStore} from 'redux';
+// import {combineReducers, createStore} from 'redux';
+// import rootReducer from './reducer';
+// import themeReducer from './themeReducer';
+
+// export const store = createStore(combineReducers({rootReducer, themeReducer}));
+
+import { combineReducers, createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import storage from 'redux-persist/lib/storage'
+
 import rootReducer from './reducer';
 import themeReducer from './themeReducer';
 
-export const store = createStore(combineReducers({rootReducer, themeReducer}));
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+//   whitelist: ['reducer', 'themeReducer']
+};
+
+const persistedReducer = persistReducer(persistConfig, combineReducers({
+  rootReducer,
+  themeReducer
+}));
+
+export const store = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
